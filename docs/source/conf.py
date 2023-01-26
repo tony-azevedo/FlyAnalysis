f01117f78297241e5a8ffd1dfa269f1d936f634f
Anthony Azevedo <tony-azevedo@users.noreply.github.com>
2023-01-26 11:52:55 -0800

    hacking rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FlyAnalysis'
+copyright = '2022, Azevedo'
+author = 'Azevedo'
 
 release = '0.1'
 version = '0.1.0'
